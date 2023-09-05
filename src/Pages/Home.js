9d0c41b95fd1d42611ea555c828277edd8b03c8e
NotesEraa <131258502+NotesEraa@users.noreply.github.com>
2023-09-06 01:26:20 +0530

    Update Home.js

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -428,6 +428,16 @@ export default function Home() {
                       <span>Media</span>
                     </button>
                   </li>
+                        //
+                         </li>
+                  <li role="presentation">
+                    <button data-bs-toggle="tab" data-bs-target="#teb_marketing" type="button" role="tab"
+                      aria-selected="false">
+                      <i className="fas fa-lightbulb-on"></i>
+                      <span>database</span>
+                    </button>
+                  </li>
+   
                   <li role="presentation">
                     <button data-bs-toggle="tab" data-bs-target="#teb_design" type="button" role="tab"
                       aria-selected="false">
